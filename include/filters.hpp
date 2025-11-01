8e2e6f54ff03c0788626acac2caaf324ecd624db
JustAntoshka <anton.tyugaevich@gmail.com>
2025-11-01 03:11:01 +0300

    implement statistics and filters

--- include/filters.hpp
+++ include/filters.hpp
@@ -8,4 +8,43 @@
 
 namespace bookdb {
 
+    auto YearBetween(int from, int to) {
+        return [from, to](const Book& book) {
+            return from < book.year && book.year < to;
+        };
+    }
+
+    auto RatingAbove(auto min_rating) {
+        return [min_rating](const Book& book){
+            return min_rating < book.rating;
+        };
+    }
+
+    auto GenreIs(auto genre) {
+        return [genre](const Book& book){
+            return book.genre == genre;
+        };
+    }
+
+    template <BookPredicate... Preds>
+    auto all_of(Preds... preds) {
+        return [preds...](const Book& b){
+            return (... && preds(b));
+        };
+    }
+
+    template <BookPredicate... Preds>
+    auto any_of(Preds... preds) {
+        return [preds...](const Book& b){
+            return (... || preds(b));
+        };
+    }
+
+    template <BookIterator Iter, BookPredicate Predicate>
+    auto filterBooks(Iter begin, Iter end, Predicate pred) {
+        std::vector<std::reference_wrapper<const Book>> result;
+        std::for_each(begin, end, [&result, pred](const auto& book){if(pred(book)) result.push_back(book);});
+        return result;
+    }
+
 }  // namespace bookdb
--- include/statsistics.hpp
+++ include/statsistics.hpp
@@ -12,7 +12,49 @@
 
 namespace bookdb {
 
-template <BookContainerLike T, typename Comparator = TransparentStringLess>
-auto buildAuthorHistogramFlat(const BookDatabase<T> &cont, Comparator comp = {}) {}
+template <BookContainerLike T, BookComparator Comparator = TransparentStringLess>
+auto buildAuthorHistogramFlat(const BookDatabase<T> &cont, Comparator comp = {}) {
+    std::flat_map<std::string, size_t> result;
+    std::for_each(cont.begin(), cont.end(), [&result](auto &book) { result[std::string(book.author)]++; });
+    return result;
+}
+
+template <BookIterator Iter>
+auto calculateGenreRatings(Iter begin, Iter end) {
+    std::flat_map<Genre, std::pair<double, size_t>> genre_2_rating_sum_map;
+    std::for_each(begin, end, [&genre_2_rating_sum_map](auto &&book) {
+        auto &[rating, n] = genre_2_rating_sum_map[book.genre];
+        rating += book.rating;
+        n++;
+    });
+    std::flat_map<Genre, double> result;
+    std::for_each(genre_2_rating_sum_map.begin(), genre_2_rating_sum_map.end(), [&result](auto &&genre_2_rating_sum) {
+        auto genre = genre_2_rating_sum.first;
+        const auto &rating_sum = genre_2_rating_sum.second;
+        result[genre] = rating_sum.first / rating_sum.second;
+    });
+    return result;
+}
+
+template <BookContainerLike T>
+auto calculateAverageRating(const BookDatabase<T> &cont) {
+    double rating =
+        std::accumulate(cont.begin(), cont.end(), 0, [](auto acc, auto book) { return acc += book.rating; });
+    return rating / cont.size();
+}
+
+template <BookContainerLike T>
+auto sampleRandomBooks(const BookDatabase<T> &cont, size_t n) {
+    std::vector<std::reference_wrapper<const Book>> result;
+    result.reserve(n);
+    std::for_each_n(cont.begin(), n, [&result](const auto &book) { result.push_back(book); });
+    return result;
+}
+
+template <BookContainerLike T, BookComparator Comparator = comp::LessByAuthor>
+auto getTopNBy(BookDatabase<T> &cont, size_t n, Comparator comp) {
+    std::sort(cont.begin(), cont.end(), comp);
+    return sampleRandomBooks(cont, n);
+}
 
 }  // namespace bookdb
